fc6a7e0d0d1224187098ba4ee6e1634acaa4a385
Regrad <regradius@gmail.com>
2026-06-02 19:33:38 +0300

    server: improve checkpoint reuse heuristics for recurrent/hybrid models

--- common/common.h
+++ common/common.h
@@ -1054,6 +1054,7 @@ struct common_prompt_checkpoint {
 
     llama_pos pos_min;
     llama_pos pos_max;
+    llama_pos pos_end;
 
     std::vector<uint8_t> data_tgt;
     std::vector<uint8_t> data_dft;
@@ -1066,7 +1067,8 @@ struct common_prompt_checkpoint {
     void update_pos(
             int64_t n_tokens,
             llama_pos pos_min,
-            llama_pos pos_max);
+            llama_pos pos_max,
+            llama_pos pos_end);
 
     void update_tgt(
             llama_context * ctx,
